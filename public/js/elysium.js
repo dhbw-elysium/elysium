0147178b0ee1e573159287ceacb27563e022fb93
theoboldt <e.s.t.m52@gmx.de>
2014-11-29 16:48:06 +0100

    Aligned code

--- public/js/elysium.js
+++ public/js/elysium.js
@@ -1,23 +1,24 @@
 $('#modalCourse').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget),
-  	  cid = button.data('cid'),
-	  cgid = button.data('cgid'),
-	  title = button.data('title'),
-  	  modal = $(this);
+	var button = $(event.relatedTarget),
+		cid = button.data('cid'),
+		cgid = button.data('cgid'),
+		title = button.data('title'),
+		modal = $(this);
 
-  modal.find('.modal-title').text('Kurs bearbeiten: ' + title);
-  modal.find('.modal-body #course-cid').val(cid);
-  modal.find('.modal-body #course-cgid').val(cgid);
-  modal.find('.modal-body #course-title').val(title);
-})
+	modal.find('.modal-title').text('Kurs bearbeiten: ' + title);
+	modal.find('.modal-body #course-cid').val(cid);
+	modal.find('.modal-body #course-cgid').val(cgid);
+	modal.find('.modal-body #course-title').val(title);
+
+});
 
 $('#modalCourseGroup').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget),
-	  cgid = button.data('cgid'),
-	  title = button.data('title'),
-  	  modal = $(this);
+	var button = $(event.relatedTarget),
+		cgid = button.data('cgid'),
+		title = button.data('title'),
+		modal = $(this);
 
-  modal.find('.modal-title').text('Themenbereich bearbeiten: ' + title);
-  modal.find('.modal-body #course-cgid').val(cgid);
-  modal.find('.modal-body #course-group-title').val(title);
-})
+	modal.find('.modal-title').text('Themenbereich bearbeiten: ' + title);
+	modal.find('.modal-body #course-cgid').val(cgid);
+	modal.find('.modal-body #course-group-title').val(title);
+});
