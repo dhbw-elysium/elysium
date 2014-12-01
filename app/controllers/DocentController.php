55d34a4f30cb1200f42a0e669af3dafe7a8131c8
theoboldt <e.s.t.m52@gmx.de>
2014-12-01 21:27:36 +0100

    Removed importer which was added for testing purpose

--- app/controllers/DocentController.php
+++ app/controllers/DocentController.php
@@ -2,12 +2,6 @@
 class DocentController extends BaseController {
 
     public function showDocent() {
-
-
-$importer	= new Elysium\Import\ImportDocents();
-    print_r($importer);
-
-
         return View::make('docent');
     }
 }
