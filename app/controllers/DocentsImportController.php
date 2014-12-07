1b8f9aad315e146b8fa21769d65a3294598f8a87
theoboldt <e.s.t.m52@gmx.de>
2014-12-07 20:12:30 +0100

    Import form for docents (draft)

--- app/controllers/DocentsImportController.php
+++ app/controllers/DocentsImportController.php
@@ -20,10 +20,99 @@ class DocentsImportController extends BaseController {
 				$parser	= DocentParser::fromFile($file->getRealPath());
 				$docents	= $parser->docents();
 
-				return View::make('docents.importprocess')->with('docents', $docents);
+				$posted	= (bool)(Input::old('posted') == 1);
+
+				Form::macro('docentLabel', function($id, $property, $title) {
+					return '<label for="docent['.$id.']['.$property.']" class="col-md-4 control-label">'.$title.'</label>';
+				});
+
+
+				$inputTemplate	= '<input class="form-control" placeholder="(leer)" name="%s" type="%s" value="%s">';
+				$groupTemplate	= '
+					<div class="col-sm-6 col-lg-6">
+						<div class="form-group">
+							<label for="%s" class="col-md-4 control-label">%s</label>
+							<div class="col-md-8">
+								%s
+							</div>
+						</div>
+					</div>';
+				Form::macro('docentBlock', function($id, $property, $title) use ($docents, $posted, $inputTemplate, $groupTemplate) {
+					$data			= $docents[$id]->data($property);
+					$elementKey		= sprintf('docent[%d][%s]', $id, $property);
+					$element		= '';
+
+					if (is_array($data)) {
+						switch($property) {
+							case 'phone_number_company':
+							case 'phone_number_private':
+								$element	= '';
+
+								$elementTemplate	= '<label for="%s" class="control-label">%s:</label>
+										<input class="form-control" placeholder="(leer)" name="%s" type="text" value="%s">';
+
+								foreach($data as $type => $number) {
+									$elementSubKey	= $elementKey.'['.$type.']';
+
+									switch($type) {
+										case 'phone':
+											$typeTitle	= 'Festnetz';
+											break;
+										case 'mobile':
+											$typeTitle	= 'Mobil';
+											break;
+										case 'fax':
+											$typeTitle	= 'Fax';
+											break;
+										default:
+											throw new InvalidArgumentException('Unknown phone number type "'.$type.'" transmitted');
+									}
+
+									$element	.= sprintf(
+										$elementTemplate,
+										$elementSubKey,
+										$typeTitle,
+										$elementSubKey,
+										(($posted) ? Input::old($elementSubKey) : $number)
+								);
+
+								}
+								break;
+							case 'private_address':
+							case 'company_address':
+								$element	= '
+									<label for="'.$elementKey.'[street]" class="control-label">Straße, Hausnummer:</label>
+									<input class="form-control" placeholder="(leer)" name="'.$elementKey.'[street]" type="text" value="'.(($posted) ? Input::old($elementKey.'[street]') : $data['street']).'">
+
+									<label for="'.$elementKey.'[plz]" class="control-label">PLZ:</label>
+									<input class="form-control" placeholder="(leer)" name="'.$elementKey.'[plz]" type="text" value="'.(($posted) ? Input::old($elementKey.'[plz]') : $data['plz']).'">
+
+									<label for="'.$elementKey.'[city]" class="control-label">Ort:</label>
+									<input class="form-control" cityholder="(leer)" name="'.$elementKey.'[city]" type="text" value="'.(($posted) ? Input::old($elementKey.'[city]') : $data['city']).'">
+									';
+
+								break;
+							case 'birth_day':
+							default:
+								foreach($data as $subKey => $value) {
+									$elementSubKey	= $elementKey.'['.$subKey.']';
+									$element		.= sprintf($inputTemplate, $elementSubKey, 'text', (($posted) ? Input::old($elementSubKey) : $value));
+								}
+								break;
+						}
+
+					} else {
+						$elementValue	= ($posted) ? Input::old($elementKey) : $data;
+						$element		= sprintf($inputTemplate, $elementKey, 'text', $elementValue);
+					}
+						return sprintf($groupTemplate, $elementKey, $title, $element);
+				});
+
+
+				return View::make('docents.importprocess')->with('docents', $docents)->with('posted', $posted);
 
 			} else {
-				return View::make('docents.importprocess')->with('danger', 'Import Fehlgeschlagen');
+				return View::make('docents.importprocess')->with('docents', array())->with('danger', 'Import Fehlgeschlagen');
 			}
 		}
 
--- app/views/docents/importprocess.blade.php
+++ app/views/docents/importprocess.blade.php
@@ -18,6 +18,77 @@
 </ul>
 
 
+{{ Form::open(array('url' => 'docents/import/process')) }}
+{{ Form::hidden('posted', '1') }}
 
+
+@if (count($docents))
+	@foreach ($docents as $id => $docent)
+
+<div class="panel panel-default" style="padding: 1em;">
+	<div class="container-fluid">
+	  <div class="form-horizontal" role="form">
+
+		<div class="row">
+			<div class="form-group">
+			  <div class="col-md-12">
+				<input type="checkbox" id="docent[{{$id}}][include]" checked="checked"> <label for="docent[{{$id}}][include]" class="control-label"> Diesen Dozenten-Datensatz beim Import mit aufnehmen</label>
+
+		  	</div>
+	  	</div>
+
+		<div class="row">
+			{{ Form::docentBlock($id, 'salution', 'Anrede:') }}
+			{{ Form::docentBlock($id, 'title', 'Titel:') }}
+
+			{{ Form::docentBlock($id, 'first_name', 'Vorname:') }}
+			{{ Form::docentBlock($id, 'last_name', 'Nachname:') }}
+
+			{{ Form::docentBlock($id, 'graduation', 'Abschluss:') }}
+			{{ Form::docentBlock($id, 'lbv', 'LBV-Nummer:') }}
+
+
+			{{ Form::docentBlock($id, 'email', 'E-Mail:') }}
+			{{ Form::docentBlock($id, 'website', 'Webseite:') }}
+
+			{{ Form::docentBlock($id, 'birth_day', 'Geburstdatum:') }}
+			{{ Form::docentBlock($id, 'birth_place', 'Geburtsort:') }}
+
+
+
+
+
+			{{ Form::docentBlock($id, 'phone_number_private', 'Telefon (Privat)') }}
+			{{ Form::docentBlock($id, 'phone_number_company', 'Telefon (Geschäftl.)') }}
+
+			{{ Form::docentBlock($id, 'private_address', 'Privat-Adresse') }}
+			{{ Form::docentBlock($id, 'company_address', 'Firmen-Adresse') }}
+
+
+			{{ Form::docentBlock($id, 'company_job', 'Beruf:') }}
+
+		</div><!-- /.row this actually does not appear to be needed with the form-horizontal -->
+	  </div><!-- form -->
+	  <p>Note: label text will occupy as much space as the text takes regardless of the
+		  column size, so be sure to validate your spacing.
+	  </p>
+	</div><!-- /.container -->
+</div>
+</div>
+
+	@endforeach
+
+
+<div class="panel panel-default">
+  <div class="panel-body">
+		{{ Form::submit('Importieren', array('class' => 'btn btn-primary')) }}
+  </div>
+</div>
+@else
+<div class="panel panel-default" style="padding: 1em;">
+	<p>In der übertragenen Datei konnten keine Dozenten gefunden werden</p>
+</div>
+@endif
+{{ Form::close() }}
 
 @stop
