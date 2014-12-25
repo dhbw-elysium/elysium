a15181a687cca6e8ed6a049e7616530350dfac28
theoboldt <e.s.t.m52@gmx.de>
2014-12-25 15:52:30 +0100

    Added constans which represent the enum field type (of phone number)

--- app/models/PhoneNumber.php
+++ app/models/PhoneNumber.php
@@ -8,6 +8,22 @@ class PhoneNumber extends Eloquent implements RemindableInterface {
 	use RemindableTrait;
 
 	/**
+	 * The value of the enum field for the phone number type phone
+	 */
+	const TYPE_PHONE	= 'phone';
+
+	/**
+	 * The value of the enum field for the phone number type mobile
+	 */
+	const TYPE_MOBILE	= 'mobile';
+
+	/**
+	 * The value of the enum field for the phone number type fax
+	 */
+	const TYPE_FAX	= 'fax';
+
+
+	/**
 	 * The database table used by the model.
 	 *
 	 * @var string
