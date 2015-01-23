b752dcb20a3aae5551a85c2054bfb20720737a66
theoboldt <e.s.t.m52@gmx.de>
2015-01-23 21:49:31 +0100

    It was impossible to create new docentStatus. Fixed

--- app/controllers/DocentController.php
+++ app/controllers/DocentController.php
@@ -411,7 +411,7 @@ class DocentController extends BaseController {
      * Track a status change of a docent
      */
     public function updateStatusEntry() {
-        if (Input::has('dsid')) {
+        if (Input::has('dsid') && Input::get('dsid') != 0) {
             $status = DocentStatus::find(Input::get('dsid'));
 
             if (!$status) {
