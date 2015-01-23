c1891a9ed3399944a06748dbba07b610273ec5ce
aschaeufler <wi12154@lehre.dhbw-stuttgart.de>
2015-01-23 13:00:50 +0100

    Changed style of the navigation menu in the status list.

--- app/views/status/list-trash.blade.php
+++ app/views/status/list-trash.blade.php
@@ -31,61 +31,80 @@
   </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 
+
 <div class="row">
 	<div class="panel-body">
-	<nav class="navbar navbar-default" role="navigation">
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="list">aktiv </a></li>
-        <li class="active"><a href="#">inaktiv (Papierkorb) <span class="sr-only">(current)</span></a></li>
-	  </ul>
-	</div>
-	</nav>
 
-		<table class="table table-striped table-grid table-hover">
-		  <thead>
-			<tr>
-				<th class="row-id">#</th>
-				<th>Symbol</th>
-				<th>Titel</th>
-				<th>Beschreibung</th>
-				<th class="row-action">
-				</th>
-			</tr>
-		  </thead>
-		  <tbody>
-			@if (count($statusList	= Status::onlyTrashed()->orderBy('title')->paginate(15)))
-				@foreach ($statusList as $status)
-				<tr>
-					<td class="row-id">{{{$status->sid}}}</td>
-					<td>
-						@if($status->glyph)
-							<i class="glyphicon {{{$status->glyph}}}"></i>
-						@else
-							<i class="text-label-empty">(keines)</i>
-						@endif
-					</td>
-					<td>{{{$status->title}}}</td>
-					<td>{{{$status->description}}}</td>
-					<td class="row-action">
 
-						<button type="button" class="btn btn-default btn-xs" data-toggle="modal" data-target="#modalStatusRestore" data-sid="{{{$status->sid}}}" data-title="{{$status->title}}">
-						  <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-						</button>
-					</td>
-				</tr>
-				@endforeach
-			@else
-				<tr>
-					<td colspan="4"><i>Keine Status im Papierkorb vorhanden</i></td>
-				</tr>
-			@endif
-		  </tbody>
-		</table>
-		<div>
-			{{ $statusList->links() }}
-		</div>
+	<div role="tabpanel">
 
-</div>
-</div>
+      <!-- Nav tabs -->
+      <ul class="nav nav-tabs" role="tablist">
+        <li role="presentation"><a href="list" aria-controls="statusActive" role="tab">Status aktiv</a></li>
+        <li role="presentation"  class="active"><a href="#statusInactive" aria-controls="statusInactive" role="tab">Status inaktiv</a></li>
+
+      </ul>
+
+      <!-- Tab panes -->
+      <div class="tab-content">
+        <div role="tabpanel" class="tab-pane" id="statusActive">
+         </div>
+                <div role="tabpanel" class="tab-pane active" id="statusInactive">
+
+                <table class="table table-striped table-grid table-hover">
+                		  <thead>
+                			<tr>
+                				<th class="row-id">#</th>
+                				<th>Symbol</th>
+                				<th>Titel</th>
+                				<th>Beschreibung</th>
+                				<th class="row-action">
+                				</th>
+                			</tr>
+                		  </thead>
+                		  <tbody>
+                			@if (count($statusList	= Status::onlyTrashed()->orderBy('title')->paginate(15)))
+                				@foreach ($statusList as $status)
+                				<tr>
+                					<td class="row-id">{{{$status->sid}}}</td>
+                					<td>
+                						@if($status->glyph)
+                							<i class="glyphicon {{{$status->glyph}}}"></i>
+                						@else
+                							<i class="text-label-empty">(keines)</i>
+                						@endif
+                					</td>
+                					<td>{{{$status->title}}}</td>
+                					<td>{{{$status->description}}}</td>
+                					<td class="row-action">
+
+                						<button type="button" class="btn btn-default btn-xs" data-toggle="modal" data-target="#modalStatusRestore" data-sid="{{{$status->sid}}}" data-title="{{$status->title}}">
+                						  <span class="glyphicon glyphicon-transfer" aria-hidden="true"></span>
+                						</button>
+                					</td>
+                				</tr>
+                				@endforeach
+                			@else
+                				<tr>
+                					<td colspan="4"><i>Kein Status im Papierkorb vorhanden</i></td>
+                				</tr>
+                			@endif
+                		  </tbody>
+                		</table>
+                		<div>
+                			{{ $statusList->links() }}
+                		</div>
+
+                </div>
+              </div>
+
+            </div>
+
+
+
+
+
+
+    </div>
+    </div>
 @stop
--- app/views/status/list.blade.php
+++ app/views/status/list.blade.php
@@ -33,63 +33,78 @@
 
 <div class="row">
 	<div class="panel-body">
-	<nav class="navbar navbar-default" role="navigation">
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">aktiv <span class="sr-only">(current)</span></a></li>
-        <li><a href="list-trash">inaktiv (Papierkorb)</a></li>
-	  </ul>
-	</div>
-	</nav>
 
-		<table class="table table-striped table-grid table-hover">
-		  <thead>
-			<tr>
-				<th class="row-id">#</th>
-				<th>Symbol</th>
-				<th>Titel</th>
-				<th>Beschreibung</th>
-				<th class="row-action">
-					<a class="btn btn-default btn-add btn-xs" href="new" data-toggle="tooltip" data-placement="left" title="Neuen Status hinzufügen">
-					  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-					</a>
-				</th>
-			</tr>
-		  </thead>
-		  <tbody>
-			@if (count($statusList	= Status::orderBy('title')->paginate(15)))
-				@foreach ($statusList as $status)
-				<tr>
-					<td class="row-id">{{{$status->sid}}}</td>
-					<td>
-						@if($status->glyph)
-							<i class="glyphicon {{{$status->glyph}}}"></i>
-						@else
-							<i class="text-label-empty">(keines)</i>
-						@endif
-					</td>
-					<td>{{{$status->title}}}</td>
-					<td>{{{$status->description}}}</td>
-					<td class="row-action">
-						<a class="btn btn-default btn-xs" href="edit/{{{$status->sid}}}" data-toggle="tooltip" data-placement="left" title="Status bearbeiten">
-						  <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-						</a>
-						<button type="button" class="btn btn-default btn-remove btn-xs" data-toggle="modal" data-target="#modalStatusDelete" data-sid="{{{$status->sid}}}" data-title="{{$status->title}}"  {{($status->sid == Status::STATUS_IMPORT) ? 'disabled' : ''}}>
-						  <span class="glyphicon glyphicon glyphicon-trash" aria-hidden="true"></span>
-						</button>
-					</td>
-				</tr>
-				@endforeach
-			@else
-				<tr>
-					<td colspan="4"><i>Keine Status vorhanden</i></td>
-				</tr>
-			@endif
-		  </tbody>
-		</table>
-		<div>
-			{{ $statusList->links() }}
-		</div>
+
+	<div role="tabpanel">
+
+      <!-- Nav tabs -->
+      <ul class="nav nav-tabs" role="tablist">
+        <li role="presentation" class="active"><a href="#statusActive" aria-controls="statusActive" role="tab">Status aktiv</a></li>
+        <li role="presentation"><a href="list-trash" aria-controls="statusInactive" role="tab">Status inaktiv</a></li>
+
+      </ul>
+
+      <!-- Tab panes -->
+      <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="statusActive">
+            <table class="table table-striped table-grid table-hover">
+        		  <thead>
+        			<tr>
+        				<th class="row-id">#</th>
+        				<th>Symbol</th>
+        				<th>Titel</th>
+        				<th>Beschreibung</th>
+        				<th class="row-action">
+        					<a class="btn btn-default btn-add btn-xs" href="new" data-toggle="tooltip" data-placement="left" title="Neuen Status hinzufügen">
+        					  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+        					</a>
+        				</th>
+        			</tr>
+        		  </thead>
+        		  <tbody>
+        			@if (count($statusList	= Status::orderBy('title')->paginate(15)))
+        				@foreach ($statusList as $status)
+        				<tr>
+        					<td class="row-id">{{{$status->sid}}}</td>
+        					<td>
+        						@if($status->glyph)
+        							<i class="glyphicon {{{$status->glyph}}}"></i>
+        						@else
+        							<i class="text-label-empty">(keines)</i>
+        						@endif
+        					</td>
+        					<td>{{{$status->title}}}</td>
+        					<td>{{{$status->description}}}</td>
+        					<td class="row-action">
+        						<a class="btn btn-default btn-xs" href="edit/{{{$status->sid}}}" data-toggle="tooltip" data-placement="left" title="Status bearbeiten">
+        						  <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+        						</a>
+        						<button type="button" class="btn btn-default btn-remove btn-xs" data-toggle="modal" data-target="#modalStatusDelete" data-sid="{{{$status->sid}}}" data-title="{{$status->title}}"  {{($status->sid == Status::STATUS_IMPORT) ? 'disabled' : ''}}>
+        						  <span class="glyphicon glyphicon glyphicon-trash" aria-hidden="true"></span>
+        						</button>
+        					</td>
+        				</tr>
+        				@endforeach
+        			@else
+        				<tr>
+        					<td colspan="4"><i>Keine Status vorhanden</i></td>
+        				</tr>
+        			@endif
+        		  </tbody>
+        		</table>
+        		<div>
+        			{{ $statusList->links() }}
+        		</div>
+        </div>
+        <div role="tabpanel" class="tab-pane" id="statusInactive">
+
+        </div>
+      </div>
+
+    </div>
+
+
+
 
 </div>
 </div>
